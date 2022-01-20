28e9056928a8822819c597a25fe0b645eac5eeb1
marco ferreira <marco@smartinnovation.com.br>
2022-01-20 18:49:11 -0300

    finally

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,6 +1,7 @@
 import { Transferencia } from './../models/transferencia-model';
 import { TransferenciaService } from './../services/transferencia.service';
 import { Component, EventEmitter, Output } from "@angular/core";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -14,7 +15,7 @@ export class NovaTransferenciaComponent {
   valor: number;
   destino: number;
 
-  constructor(private service: TransferenciaService) {}
+  constructor(private service: TransferenciaService, private router: Router) {}
 
   transferir() {
     // posso passar os valores numa variavel como objeto const valorEmitir = {valor: this.valor, destino: this.destino}
@@ -24,6 +25,7 @@ export class NovaTransferenciaComponent {
     this.service.adicionar(valorEmitir).subscribe(resultado => {
       console.log(resultado);
       this.limparCampos();
+      this.router.navigateByUrl('extrato');
     }, error => console.error(error))
 
     this.limparCampos();
